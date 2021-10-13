8a1cdda693f49539e59fc8936d7fff7669b6880f
Ubuntu <ubuntu@ip-172-31-36-214.us-east-2.compute.internal>
2021-10-13 02:08:27 +0000

    this is add.c file commit

---
+++ add.c
@@ -0,0 +1,11 @@
+//this is my program
+#include<stdio.h>
+int main()
+{
+	int a , b , res;
+	printf("Enter 2 noumbers:");
+	scanf("%d%d",&a,&b);
+	res = a + b;
+	printf("the res = %d\n",res);
+	return 0;
+}
